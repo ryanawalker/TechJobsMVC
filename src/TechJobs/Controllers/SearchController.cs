6ef0e2bebcb33a061e8c36e25cd33ae8617a1d65
Ryan A. Walker <rawn8c@mail.umsl.edu>
2017-03-23 12:47:29 -0500

    Completes solution.

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using TechJobs.Models;
+using System;
 
 namespace TechJobs.Controllers
 {
@@ -15,6 +16,23 @@ namespace TechJobs.Controllers
 
         // TODO #1 - Create a Results action method to process 
         // search request and display results
+
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            ViewBag.columns = ListController.columnChoices;
+            if (searchType == "all")
+            {
+                List<Dictionary<String, String>> jobs = JobData.FindByValue(searchTerm);
+                ViewBag.jobs = jobs;
+            }
+            else
+            {
+                List<Dictionary<String, String>> jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+                ViewBag.jobs = jobs;
+            }
+            ViewBag.title = "Search Results for " + searchType + " containing " + searchTerm + ".";
+            return View("Index");
+        }
 
     }
 }
